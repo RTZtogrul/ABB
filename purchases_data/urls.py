d04c132d27ae9a408e6e5fc0378f6415d0d0baae
togrul <totush04@mail.ru>
2022-07-22 12:06:18 +0400

    resful

--- purchases_data/urls.py
+++ purchases_data/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from rest_framework import routers
+from .views import PurchaseViewSet  
+
+router = routers.DefaultRouter()
+router.register('purchases', PurchaseViewSet)
+
+
 
 urlpatterns = [
-    path('', home, name='home')
-]
+] + router.urls
--- purchases_data/views.py
+++ purchases_data/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .serializer import PurchaseSerializer
+from .models import Purchase 
+from rest_framework import viewsets
 
 def home(request):
     return render(request, "purchases_data/purchase.html")
+
+
+class PurchaseViewSet(viewsets.ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseSerializer
